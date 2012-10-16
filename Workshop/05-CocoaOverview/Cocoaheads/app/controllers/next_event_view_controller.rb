3f16180057aaeaa67aa2c792808aa648ca239b0d
Erick Camacho <ecamacho.mx@gmail.com>
2012-10-16 18:23:59 -0500

    added background image

--- Workshop/05-CocoaOverview/Cocoaheads/app/controllers/next_event_view_controller.rb
+++ Workshop/05-CocoaOverview/Cocoaheads/app/controllers/next_event_view_controller.rb
@@ -1,9 +1,17 @@
 class NextEventViewController < UIViewController
   
+  def init
+    p 'Initializing NextEventViewController'
+    super
+    self
+  end  
 
   def loadView
+    p 'Loading View'
     self.view = UIView.alloc.init
     self.view.backgroundColor = UIColor.colorWithPatternImage( UIImage.imageNamed( "handmadepaper" ))
+    
+    self.view.addSubview( imageViewWithStars )
 
     @next_event_name_label = labelWithNextEventName
     @days_left_label = labelWithDaysLeft
@@ -12,6 +20,8 @@ class NextEventViewController < UIViewController
     
     self.view.addSubview( buttonForSignIn )
     self.view.addSubview( buttonForSignUp )
+
+    
   end
 
   def labelWithNextEventName
@@ -20,8 +30,9 @@ class NextEventViewController < UIViewController
     next_event_name_label.font = UIFont.fontWithName("Helvetica", size:22)
     next_event_name_label.backgroundColor = UIColor.clearColor
     next_event_name_label.textAlignment = UITextAlignmentCenter
-
+    next_event_name_label.textColor = UIColor.colorWithRed(170.0/255.0, green:32.0/255.0, blue:0, alpha:1.0)
     next_event_name_label
+
   end
 
   def labelWithDaysLeft
@@ -31,36 +42,66 @@ class NextEventViewController < UIViewController
     days_left_label
   end  
 
+  def imageViewWithStars
+    stars_imageview = UIImageView.alloc.initWithImage( UIImage.imageNamed('stars.png') )
+    stars_imageview.alpha = 0.4
+    stars_imageview
+  end  
+
   def buttonForSignIn
     sign_in_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    sign_in_button.frame = [[15, 250], [295, 40]]
+    sign_in_button.frame = [[15, 280], [295, 40]]
     sign_in_button.setTitle("I have an account, sign-in to book", forState:UIControlStateNormal)
     sign_in_button.setTitle("is Highlighted", forState:UIControlStateHighlighted)
 
     sign_in_button.addTarget(self, action:'sign_in:', forControlEvents:UIControlEventTouchUpInside)
+    sign_in_button.tintColor = UIColor.yellowColor
+    sign_in_button
   end  
 
   def buttonForSignUp
-    sign_up_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    sign_up_button.frame = [[15, 310], [295, 40]]
+    sign_up_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)    
+    sign_up_button.frame = [[15, 350], [295, 40]]    
     sign_up_button.setTitle("Don't have an account, sign-up", forState:UIControlStateNormal)
+    sign_up_button.tintColor = UIColor.redColor
+    sign_up_button
   end  
     
   def viewDidLoad
+    p 'viewDidLoad'
     super
     @days_left_label.text = "20 Days Left"
     @next_event_name_label.text = "November meeting."    
   end
 
+  def viewWillAppear( animated )
+    p 'viewWillAppear'
+    super
+  end  
+
+  def viewDidAppear( animated )
+    p 'viewDidAppear'
+    super
+  end  
+
+
+  def viewWillDisappear( animated )
+    p 'viewWillDisappear'
+    super
+  end
+
+
+
   def viewDidUnload
+    p 'viewDidUnload'
     super
     # Release any retained subviews of the main view.
   end
 
   
 
-
   def shouldAutorotateToInterfaceOrientation(interfaceOrientation)
+    #change this to enable rotation to landscape orientation    
     interfaceOrientation == UIInterfaceOrientationPortrait
   end
 
