bd65da215fb1903233efb616d48945c3701654eb
Erick Camacho <ecamacho.mx@gmail.com>
2012-10-17 12:13:31 -0500

    added small mapview

--- Workshop/07-MapKit/Cocoaheads/app/controllers/event_detail_view_controller.rb
+++ Workshop/07-MapKit/Cocoaheads/app/controllers/event_detail_view_controller.rb
@@ -1,6 +1,5 @@
 class EventDetailViewController < UIViewController
 
-
   def loadView
     self.view = UIView.alloc.init
     self.view.backgroundColor = UIColor.colorWithPatternImage( UIImage.imageNamed( "handmadepaper" ))
@@ -9,16 +8,21 @@ class EventDetailViewController < UIViewController
     @first_talk_label = labelForFirstTalk
     @second_talk_label = labelForSecondTalk
     @location_label = labelForLocation
-
+    @map_view_for_event = mapViewForEvent
+    
     self.view.addSubview( @event_date_label )
     self.view.addSubview( @event_address_label )
     self.view.addSubview( @first_talk_label )
     self.view.addSubview( @second_talk_label )
     self.view.addSubview( @location_label )
+    self.view.addSubview( @map_view_for_event )
+    self.view.addSubview( buttonForMap )
+    self.view.addSubview( buttonForBooking )
+    p 'loadview'
   end  
     
   def labelForEventDate
-    event_date_label = UILabel.alloc.initWithFrame([ [25,20], [140, 100]])
+    event_date_label = UILabel.alloc.initWithFrame([ [25,10], [140, 100]])
     event_date_label.numberOfLines = 2
     event_date_label.backgroundColor = UIColor.clearColor
     event_date_label.font = UIFont.fontWithName('Helvetica', size:16.0)    
@@ -26,7 +30,7 @@ class EventDetailViewController < UIViewController
   end
 
   def labelForEventAddress
-    event_address_label = UILabel.alloc.initWithFrame([ [165,20], [140, 100]])
+    event_address_label = UILabel.alloc.initWithFrame([ [165,10], [140, 100]])
     event_address_label.numberOfLines = 2
     event_address_label.backgroundColor = UIColor.clearColor
     event_address_label.font = UIFont.fontWithName('Helvetica', size:16.0)    
@@ -34,7 +38,7 @@ class EventDetailViewController < UIViewController
   end
 
   def labelForFirstTalk
-    first_talk_label = UILabel.alloc.initWithFrame([ [25,180], [200, 100]])
+    first_talk_label = UILabel.alloc.initWithFrame([ [25,200], [200, 100]])
     first_talk_label.numberOfLines = 2
     first_talk_label.backgroundColor = UIColor.clearColor
     first_talk_label.font = UIFont.fontWithName('Helvetica', size:18.0)    
@@ -42,7 +46,7 @@ class EventDetailViewController < UIViewController
   end
 
   def labelForSecondTalk
-    second_talk_label = UILabel.alloc.initWithFrame([ [25,260], [200, 100]])
+    second_talk_label = UILabel.alloc.initWithFrame([ [25,255], [200, 100]])
     second_talk_label.numberOfLines = 2
     second_talk_label.backgroundColor = UIColor.clearColor
     second_talk_label.font = UIFont.fontWithName('Helvetica', size:18.0)    
@@ -50,17 +54,43 @@ class EventDetailViewController < UIViewController
   end
 
   def labelForLocation
-    label_for_location = UILabel.alloc.initWithFrame([ [25,60], [290, 100]])
+    label_for_location = UILabel.alloc.initWithFrame([[25,80], [290, 40]])
     label_for_location.backgroundColor = UIColor.clearColor
     label_for_location.font = UIFont.fontWithName('Helvetica', size:10.0)    
     label_for_location
   end  
 
-  def buttonForBooking
+  def buttonForMap
+    button_for_map = UIButton.buttonWithType( UIButtonTypeRoundedRect )
+    button_for_map.frame = [[25, 350], [110, 40]]
+    button_for_map.setTitle("View Map", forState:UIControlStateNormal)
+    button_for_map.addTarget(self, action:'viewMap', forControlEvents:UIControlEventTouchUpInside)
     
+    button_for_map
   end  
 
+  def buttonForBooking
+    button_for_booking = UIButton.buttonWithType( UIButtonTypeRoundedRect )
+    button_for_booking.frame = [[165, 350], [110, 40]]
+    button_for_booking.setTitle("Book Event", forState:UIControlStateNormal)
+    button_for_booking.addTarget(self, action:'viewMap', forControlEvents:UIControlEventTouchUpInside)
+    
+    button_for_booking
+  end  
+
+  def viewMap
+    p 'viewMap'
+  end  
+
+  def mapViewForEvent
+
+    map_view_for_event = MKMapView.alloc.initWithFrame( [[25,130], [270, 80]] )
+    map_view_for_event.mapType = MKMapTypeSatellite    
+    map_view_for_event
+  end  
+   
   def viewDidLoad
+
     super
     self.title = "November meeting"
     @event_date_label.text = "Saturday 14th November 2012"
@@ -79,13 +109,10 @@ class EventDetailViewController < UIViewController
     interfaceOrientation == UIInterfaceOrientationPortrait
   end
 
-  
-
   def requestUserCurrenLocation
     if (CLLocationManager.locationServicesEnabled)
       @location_manager = CLLocationManager.alloc.init
       @location_manager.delegate = self
-
       @location_manager.purpose = "To provide functionality based on user's current location" 
       @location_manager.startUpdatingLocation
     else
@@ -98,7 +125,7 @@ class EventDetailViewController < UIViewController
                                                   message:message, 
                                                   delegate:nil, 
                                                   cancelButtonTitle:"Ok", 
-                                                  otherButtonTitles:nil)
+                                                  otherButtonTitles:nil )
     alert_view.show
   end 
 
@@ -108,6 +135,6 @@ class EventDetailViewController < UIViewController
   end
 
   def locationManager(manager, didFailWithError:error)
-    showAlertWithTitle( "Error", andMessage:error.description)
+    showAlertWithTitle( "Error", andMessage:error.description )
   end
 end
