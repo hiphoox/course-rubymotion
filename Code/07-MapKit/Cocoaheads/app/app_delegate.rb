5cd49cd07830831fb1647fc8eccfc4e2798fa51e
Erick Camacho <ecamacho.mx@gmail.com>
2012-11-30 16:39:41 -0600

    deleting old folder structure

--- Code/07-MapKit/Cocoaheads/app/app_delegate.rb
+++
@@ -1,9 +0,0 @@
-class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    event_detail_view_controller = EventDetailViewController.alloc.init
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = event_detail_view_controller
-    @window.makeKeyAndVisible
-    true
-  end
-end
--- Code/12-TableViews/Cocoaheads/spec/main_spec.rb
+++
@@ -1,7 +0,0 @@
-describe "Application 'Cocoaheads'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  
-end
