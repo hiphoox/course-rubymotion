729c2f4097324c54b9c648d35e3c2673c3d186d9
Juan José Karam <juan.karam@curveberyl.com>
2012-12-12 02:37:25 -0600

    Chapter 24 complete

--- Code/24-CoreData/solution/Remember the Movies/app/controller/add_director_view_controller.rb
+++ Code/24-CoreData/solution/Remember the Movies/app/controller/add_director_view_controller.rb
@@ -18,45 +18,61 @@ class AddDirectorViewController < UITableViewController
     # Add the table view as view of the view controller
     self.view = table_view
 
-
+    # Add new Bar Button Item to the Navigation Bar for saving
     save_bar_button_item = UIBarButtonItem.alloc.initWithTitle('Save',
                                                                style: UIBarButtonItemStyleDone,
                                                                target: self,
-                                                               action: 'save')
+                                                               action: 'save_new_director')
 
+    # Add the Bar Button Item to the Navigation Bar
     self.navigationItem.rightBarButtonItem = save_bar_button_item
 
+    # Disable the Save button until the user add some text
+    # into the UITextView
     self.navigationItem.rightBarButtonItem.enabled = false;
   end
 
 
-  def save
+  def save_new_director
 
+    # Using Core Data create a new instance of the object Director
     director = NSEntityDescription.insertNewObjectForEntityForName(Director.name, 
                                                                    inManagedObjectContext: @managed_object_context)
 
+    # Assign the text of the UITextField to the director name
     director.name = @name_text_field.text
  
 
+    # Create a new pointer for managing the errors
     error_pointer = Pointer.new(:object)
 
+    # Lets persist the new Director object, saving the managed
+    # object context that contains it
     unless @managed_object_context.save(error_pointer)
 
+      # In case we can not save it
       raise "Error saving a new Director: #{error_pointer[0].description}"
     end
+
+    # Pop the Director View Controller
+    self.navigationController.popViewControllerAnimated(true)
   end
 
 
   def textField(textField, shouldChangeCharactersInRange: range, replacementString: string)
 
+    # Calculate the final lenght of the text in the UITextField
     text_length = textField.text.length + string.length - range.length
 
 
+    # If the lenght of the text is greater than 0
     if text_length > 0
 
+      # Enable the save button
       self.navigationItem.rightBarButtonItem.enabled = true;
     else
 
+      # Else disable the save button
       self.navigationItem.rightBarButtonItem.enabled = false;
     end
 
@@ -90,7 +106,8 @@ class AddDirectorViewController < UITableViewController
       cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:cell_identifier)
       cell.selectionStyle = UITableViewCellSelectionStyleNone
 
-      # Instantiate a new UITextField for editing some values
+      # Instantiate a new UITextField for editing the name of the 
+      # new director
       @name_text_field = UITextField.alloc.initWithFrame(CGRectMake(100, 11, 200, 30))
       @name_text_field.autocorrectionType = UITextAutocorrectionTypeNo;
       @name_text_field.placeholder = 'Required'
--- Code/24-CoreData/solution/Remember the Movies/app/controller/add_movie_view_controller.rb
+++ Code/24-CoreData/solution/Remember the Movies/app/controller/add_movie_view_controller.rb
@@ -4,6 +4,9 @@ class AddMovieViewController < UITableViewController
 
   def loadView
 
+    #For security lets set up the fetched_directors array
+    @fetched_directors = []
+
     # Set up the title for the View Controller
     self.title = 'Add Movie'
 
@@ -21,6 +24,125 @@ class AddMovieViewController < UITableViewController
 
     # Add the table view as view of the view controller
     self.view = table_view
+
+
+    # Add new Bar Button Item to the Navigation Bar for saving
+    save_bar_button_item = UIBarButtonItem.alloc.initWithTitle('Save',
+                                                               style: UIBarButtonItemStyleDone,
+                                                               target: self,
+                                                               action: 'save_new_movie')
+
+    # Add the Bar Button Item to the Navigation Bar
+    self.navigationItem.rightBarButtonItem = save_bar_button_item
+
+    # Disable the Save button until the user add some text
+    # into the text views and select a Director from the list
+    self.navigationItem.rightBarButtonItem.enabled = false;
+  end
+
+
+  def save_new_movie
+
+    # Using Core Data create a new instance of the object Movie
+    movie = NSEntityDescription.insertNewObjectForEntityForName(Movie.name, 
+                                                                inManagedObjectContext: @managed_object_context)
+
+    # Assign the text of the name text field to the movie
+    # name
+    movie.name = @name_text_field.text
+
+    if @release_year_text_field.text != nil
+
+      # Assign the text of the year text field to the movie
+      # year
+      movie.release_year = @release_year_text_field.text.intValue
+    end
+
+    # Assign the selected director to the movie
+    movie.director = @selected_director
+
+    # Create a new pointer for managing the errors
+    error_pointer = Pointer.new(:object)
+
+    # Lets persist the new Movie object, saving the managed
+    # object context that contains it
+    unless @managed_object_context.save(error_pointer)
+
+      # In case we can not save it
+      raise "Error saving a new Director: #{error_pointer[0].description}"
+    end
+
+    # Pop the Director View Controller
+    self.navigationController.popViewControllerAnimated(true)
+  end
+
+
+  def textField(textField, shouldChangeCharactersInRange: range, replacementString: string)
+
+    # If the changing text field is the name one and there is already
+    # selected a Director from the list
+    if textField == @name_text_field && @selected_director != nil
+
+      # Calculate the final lenght of the text in the UITextField
+      text_length = textField.text.length + string.length - range.length
+
+
+      # If the lenght of the text is greater than 0
+      if text_length > 0
+
+        # Enable the save button
+        self.navigationItem.rightBarButtonItem.enabled = true;
+      else
+
+        # Else disable the save button
+        self.navigationItem.rightBarButtonItem.enabled = false;
+      end
+    end
+
+    true
+  end
+
+
+  def viewWillAppear(animated)
+
+    super
+
+    # Using a NSFetchRequest object we can ask Core Data
+    # to fetch specific objects
+    fetch_request = NSFetchRequest.alloc.init
+
+    # We need a NSEntityDescription for the Director object
+    # so we can tell Core Data which entity we want to 
+    # retrieve
+    director_entity = NSEntityDescription.entityForName(Director.name, 
+                                                        inManagedObjectContext:@managed_object_context)
+    fetch_request.setEntity(director_entity)
+
+    # Sort the directors by their "name" attribute
+    fetch_sort = NSSortDescriptor.alloc.initWithKey('name',
+                                                    ascending: true)
+    fetch_request.setSortDescriptors([fetch_sort])
+
+
+    # Create a new pointer for managing the errors
+    error_pointer = Pointer.new(:object)
+
+    # Using the NSManagedObjectContext execute the fetch
+    # request
+    @fetched_directors = @managed_object_context.executeFetchRequest(fetch_request,
+                                                                     error: error_pointer)
+
+    # If the returning array of the fetch request is nil
+    # means that a problem has occured
+    unless @fetched_directors
+
+      # In case we can not fetch the directors
+      raise "Error fetching Directors: #{error_pointer[0].description}"
+
+    end
+
+    # Ask the table view to reload its data
+    self.view.reloadData
   end
 
 
@@ -66,9 +188,10 @@ class AddMovieViewController < UITableViewController
 
     else
 
-      # If the section is Directors we need only one
-      number_of_rows = 1
-
+      # If the section is Directors we need to add one more
+      # to the total of directors fetched because the need of
+      # the 'Add Director...' cell
+      number_of_rows = @fetched_directors.count + 1
     end
 
     # Return the number of rows for the section
@@ -83,11 +206,45 @@ class AddMovieViewController < UITableViewController
 
       # Return a General Data Cell
       general_information_cell_for_table_view(tableView, index_path: indexPath)
+
+      # If the section is Directors but is not the 'Add Director...'
+      # cell, remember we are adding + 1 to the total fetched directors 
+      # count
+    elsif indexPath.section == 1 && indexPath.row < @fetched_directors.count
+
+      # Return a Director Cell
+      director_cell_for_table_view(tableView, index_path: indexPath)
     else
 
       # Return a Add Director Cell
       add_director_cell_for_table_view(tableView)
     end
+  end
+
+
+  def director_cell_for_table_view(tableView, index_path: index_path)
+
+    #Create a cell identifier for the Director Cell
+    cell_identifier = 'DirectorCell'
+
+    # Dequeue a cell with the identifier
+    cell = tableView.dequeueReusableCellWithIdentifier(cell_identifier)
+
+
+    # If we are not cells to use we need to create one
+    if cell == nil
+
+      # Lets create a new UITableViewCell with the identifier
+      cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:cell_identifier)
+    end
+
+    # Get the fetched director from the array
+    director = @fetched_directors[index_path.row]
+
+    # Add the director name to the cell
+    cell.textLabel.text = director.name
+
+    cell
   end
 
 
@@ -110,7 +267,6 @@ class AddMovieViewController < UITableViewController
       # Instantiate a new UITextField for editing some values
       cell_text_field = UITextField.alloc.initWithFrame(CGRectMake(100, 11, 200, 30))
       cell_text_field.autocorrectionType = UITextAutocorrectionTypeNo;
-      cell_text_field.placeholder = 'Required'
       cell_text_field.textColor = UIColor.colorWithRed(0.235, green:0.325, blue:0.506, alpha:1.000)
 
       # Set the view controller as delegate of the Text Field
@@ -118,6 +274,21 @@ class AddMovieViewController < UITableViewController
 
       # Add the Text Field into the cell view
       cell.addSubview(cell_text_field)
+
+      # If the row is the first one
+      if index_path.row == 0
+
+        cell_text_field.placeholder = 'Required'
+
+        # Assign the cell_text_field as name_text_field
+        @name_text_field = cell_text_field      
+      else
+
+        cell_text_field.placeholder = 'Optional'
+
+        # Assign the cell_text_field as release_year_text_field
+        @release_year_text_field = cell_text_field  
+      end
     end
 
 
@@ -175,8 +346,9 @@ class AddMovieViewController < UITableViewController
 
   def tableView(tableView, didSelectRowAtIndexPath: indexPath)
 
-    # If the section is the Directors one
-    if indexPath.section == 1 
+    # If the section is the Directors one and the cell is the
+    # 'Add Director...' one
+    if indexPath.section == 1 && indexPath.row > @fetched_directors.count - 1 
 
       # Create a new AddDirectorViewController
       add_director_view_controller = AddDirectorViewController.alloc.init
@@ -188,6 +360,20 @@ class AddMovieViewController < UITableViewController
       # Push it using the Navigation Controller
       self.navigationController.pushViewController(add_director_view_controller, 
                                                    animated:true)
+
+      # If the section is Directors but is not the 'Add Director...'
+      # cell, remember we are adding + 1 to the total fetched directors 
+      # count
+    elsif indexPath.section == 1 && indexPath.row < @fetched_directors.count 
+
+      @selected_director = @fetched_directors[indexPath.row]
+
+      # If the lenght of the text is greater than 0
+      if @name_text_field.text != nil && @name_text_field.text.length > 0
+
+        # Enable the save button
+        self.navigationItem.rightBarButtonItem.enabled = true;
+      end
     end
 
   end
